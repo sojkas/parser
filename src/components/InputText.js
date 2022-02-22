107bcb7a1b0d947a70c18623f5ad11bacefb3381
Stepan Sojka <sojka.stepan@gmail.com>
2022-02-22 15:38:40 +0100

    vystylovani textfieldu tailwind only

--- src/components/InputText.js
+++ src/components/InputText.js
@@ -8,8 +8,8 @@ const InputText = (props) =>{
 
     return (
         <div className="text-input__form">
-            <input className="text-input__input" type="text" onChange={onTextTypeHandler} placeholder={props.customTextPlaceholder}/>
-            <p className="text-input__label">{props.title}</p>            
+            <input className="border rounded p-4 focus:border-2 focus:border-navy focus:p-[calc(1rem-1px)] placeholder:text-silvergrey" type="text" onChange={onTextTypeHandler} placeholder={props.customTextPlaceholder}/>
+            <p className="text-xs p-1">{props.title}</p>            
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   content: ["./src/*.{html,js}","./src/components/*.js"],
   theme: {
     extend: {},
+    colors: {
+      'navy' : '#0366d6',
+      'silvergrey': '#586069'
+    }
   },
   plugins: [],
 }
